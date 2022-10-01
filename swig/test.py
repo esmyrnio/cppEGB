8256cf215d2bd7c52d1ac4269f8d84f3782fdff8
esmyrnio <smirnios.10@gmail.com>
2022-10-01 16:07:07 +0300

    typo fix

--- swig/test.py
+++ swig/test.py
@@ -6,8 +6,8 @@ import math
 size = 50 #number of models to be computed
 minPressure = 0.1 #(x10^35 dyn/cm^2)
 maxPressure = 30.0 #(x10^35 dyn/cm^2)
-pressures = np.logspace(math.log10(minDensity),math.log10(maxDensity),size)
-eosName = "ppwff1.cold.rns1.1.txt" #see EoS/ for other equations of state available
+pressures = np.logspace(math.log10(minPressure),math.log10(maxPressure),size)
+eosName = "ppsly4.cold.rns1.1.txt" #see EoS/ for other equations of state available
 coupling = 20.0 # km^2
 accuracy = 1e-5 # iterative method tolerance
 maxIter = 500 # maximum method iterations
